1687bab806e90e24a588d45b6a606262df49a967
giova <giovanni.aravena@avalora.com>
2020-05-24 06:48:05 -0400

    Mantenedor: modo produccion 4
    Se agrega el volumen para node_modules

--- .circleci/deploy_app.sh
+++ .circleci/deploy_app.sh
@@ -1,6 +1,6 @@
 cd /home/ohl/controlCamionesApi/
 git checkout develop
-git pull https://github.com/giovanniavalora/controlCamionesApi.git
+git pull https://github.com/giovanniavalora/controlCamionesApi.git develop
 # docker-compose -f docker-compose.prod.yml up -d --build
 #docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations --noinput
 #docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
