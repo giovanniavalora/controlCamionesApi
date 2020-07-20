c1004668af74a94b452891a5139fab2f808916ec
giova <giovanni.aravena@avalora.com>
2020-07-20 07:57:40 -0400

    deploy qa y prod

--- .circleci/deploy_app_prod.sh
+++ .circleci/deploy_app_prod.sh
@@ -1,6 +1,6 @@
 cd /home/ohl/controlCamionesApi/
-git checkout develop
-git pull https://github.com/giovanniavalora/controlCamionesApi.git develop
+git checkout master
+git pull https://github.com/giovanniavalora/controlCamionesApi.git master
 # docker-compose -f docker-compose.prod.yml up -d --build
 #docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations --noinput
 #docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
